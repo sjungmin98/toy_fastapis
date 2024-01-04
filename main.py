a4c1ae4615c550d6e996907082b3c1c7eb26cb92
sjungmin98 <shc980@gmail.com>
2024-01-04 15:54:57 +0900

    commit

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from fastapi import Request
 from fastapi import FastAPI
-from routes.gadgets import gadgets_router as gadgets_router
-from routes.positionings import positionings_router as positionings_router
-from routes.users import users_router as users_router
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
-app.include_router(gadgets_router, prefix="/gadgets")
-app.include_router(positionings_router, prefix="/positionings")
-app.include_router(users_router, prefix="/users")
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,11 +12,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from routes.gadgets import gadgets_router as gadgets_router
+from routes.positionings import positionings_router as positionings_router
+from routes.users import users_router as users_router
+
+app.include_router(gadgets_router, prefix="/gadgets")
+app.include_router(positionings_router, prefix="/positionings")
+app.include_router(users_router, prefix="/users")
+
+
+
 templates = Jinja2Templates(directory="templates/")
 
 @app.get("/")
 async def root(request:Request):
-    return templates.TemplateResponse("main.html", {'request':request})
+    return templates.TemplateResponse("/main.html", {'request':request})
 
 
 
